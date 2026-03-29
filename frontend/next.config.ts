a4e424e63ad11f3c6c6152828c8dbbf21780c452
yschaffler <yannikschaeffler@icloud.com>
2026-03-29 16:55:21 +0200

    next.config for dev testing on mobile device

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Die IP ohne Port UND mit Port hinzufügen */
+  allowedDevOrigins: ["192.168.2.118", "192.168.2.118:3000"],
 };
 
 export default nextConfig;
