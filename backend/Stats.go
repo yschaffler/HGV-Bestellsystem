d227a739fc22ba45b97332f039368369b207bae4
unimatrix665 <corvin.wernick@wernick-net.de>
2026-05-19 08:47:00 +0200

    add OrderByServerID function to generate report by KellnerID

---
+++ backend/Stats.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/johnfercher/maroto/v2"
+	"github.com/johnfercher/maroto/v2/pkg/components/text"
+	"github.com/johnfercher/maroto/v2/pkg/core"
+)
+
+func OrderByServerID() (core.Maroto, error) {
+	arr, r := getAllRechnungen(DB)
+	if r != nil {
+		return nil, r
+	}
+
+	m := maroto.New()
+	var tempmap map[string]float64 = make(map[string]float64)
+
+	m.AddRow(5,
+		text.NewCol(4, "KellnerID"),
+		text.NewCol(4, "->"),
+		text.NewCol(4, "Gesamt"),
+	)
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i].KellnerId == "" {
+			continue
+		}
+		tempmap[arr[i].KellnerId] += arr[i].Gesamt
+	}
+
+	for key, value := range tempmap {
+		m.AddRow(5,
+			text.NewCol(4, fmt.Sprintf("%v", key)),
+			text.NewCol(4, fmt.Sprintf("%v", "->")),
+			text.NewCol(4, fmt.Sprintf("%v", value)),
+		)
+	}
+	return m, nil
+}
