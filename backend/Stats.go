290598bec633b14696f2e1ebdb293c7ac6bb2e25
unimatrix665 <corvin.wernick@wernick-net.de>
2026-06-01 18:22:39 +0200

    added func to get all Stornos; expanded PDF creation

--- backend/Stats.go
+++ backend/Stats.go
@@ -9,25 +9,60 @@ import (
 )
 
 func OrderByServerID() (core.Maroto, error) {
-	arr, err := getAllRechnungen(DB)
-	if err != nil {
-		return nil, err
+	arrRechnungen, r := getAllRechnungen(DB)
+	arrStorno, r := getAllStornos(DB)
+	if r != nil {
+		return nil, r
 	}
 
 	m := maroto.New()
-	var tempmap map[string]float64 = make(map[string]float64)
 
+	//Umsatz
+	Umsatz := 0.0
+	for i := 0; i < len(arrRechnungen); i++ {
+		Umsatz += arrRechnungen[i].Gesamt
+	}
+	m.AddRow(5,
+		text.NewCol(4, "Umsatz"),
+		text.NewCol(4, "->"),
+		text.NewCol(4, fmt.Sprintf("%v", Umsatz)),
+	)
+
+	//Gesamt Stornos
+	Storno := 0.0
+	for i := 0; i < len(arrStorno); i++ {
+		Storno += arrStorno[i].Gesamt
+	}
+	m.AddRow(5,
+		text.NewCol(4, "Alle Stornos"),
+		text.NewCol(4, "->"),
+		text.NewCol(4, fmt.Sprintf("%v", Storno)),
+	)
+
+	//Anzahl Verkaufter Artikel
+	Anzahl := 0
+	for i := 0; i < len(arrRechnungen); i++ {
+		Anzahl += arrRechnungen[i].Positionen[0].Amount
+	}
+	m.AddRow(5,
+		text.NewCol(4, "Anzahl Verkaufter Artikel"),
+		text.NewCol(4, "->"),
+		text.NewCol(4, fmt.Sprintf("%v", Anzahl)),
+	)
+
+	//Kellner zuordnung zu Einnahmen
+	var tempmap map[string]float64 = make(map[string]float64)
 	m.AddRow(5,
 		text.NewCol(4, "KellnerID"),
 		text.NewCol(4, "->"),
 		text.NewCol(4, "Gesamt"),
 	)
 
-	for i := 0; i < len(arr); i++ {
-		if arr[i].KellnerId == "" {
-			continue
+	for i := 0; i < len(arrRechnungen); i++ {
+		if arrRechnungen[i].KellnerId == "" {
+			arrRechnungen[i].KellnerId = "Bar"
 		}
-		tempmap[arr[i].KellnerId] += arr[i].Gesamt
+		tempmap[arrRechnungen[i].KellnerId] += arrRechnungen[i].Gesamt
 	}
 
 	for key, value := range tempmap {
@@ -37,5 +72,6 @@ func OrderByServerID() (core.Maroto, error) {
 			text.NewCol(4, fmt.Sprintf("%v", value)),
 		)
 	}
+
 	return m, nil
 }
